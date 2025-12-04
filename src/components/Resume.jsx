683dd235cf183ca6746b65babaebdb9df10399bf
Pinkaew Horputra <pinoripin@gmail.com>
2025-12-04 16:37:10 -0500

    resume: improve PDF viewer responsiveness, titlebar and add controls wrapper

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,13 @@
 import { Document, pdfjs, Page } from "react-pdf";
 import Window from "./Window";
-import { CircleArrowDown, ChevronDown, Share, PanelLeftIcon, ZoomIn, ZoomOut, Info, Search } from "lucide-react";
+import { CircleArrowDown, ChevronDown, Share, PanelLeftIcon, ZoomIn, ZoomOut, Info, Search, Rotate3D, RotateCw } from "lucide-react";
 import useWindowsStore from "@store/window";
 import { WINDOW_IDS, apps } from "@constants";
 import { TransformWrapper, TransformComponent, useControls } from "react-zoom-pan-pinch";
 import WindowControls from "./WindowControls";
+import { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
@@ -26,16 +29,20 @@ const Controls = () => {
 };
 
 const Resume = () => {
+	const window = useWindowsStore((state) => state.windows[WINDOW_IDS.RESUME]);
+    const containerWidth = window?.width;
 	return (
-		<TransformWrapper initialScale={1} initialPositionX={0} initialPositionY={0} wheel={{ wheelDisabled: true, step: 1, smoothStep: 0.01 }} panning={{ wheelPanning: true }}>
-			<div className="resume bg-secondary mb-8 flex justify-center">
-				<TransformComponent>
-					<Document file="/files/Pin_Horputra_Resume.pdf" loading="Loading resume..." className="flex justify-center">
-						<Page pageNumber={1} width={600} renderTextLayer renderAnnotationLayer className="bg-black" />
-					</Document>
-				</TransformComponent>
-			</div>
-		</TransformWrapper>
+		<div className="bg-secondary flex justify-center items-start h-full w-full overflow-auto">
+			<TransformComponent>
+				<Document
+					file="/files/Pin_Horputra_Resume.pdf"
+					loading="Loading resume..."
+					onLoadError={(error) => console.error("Error while loading document!", error)}
+				>
+					<Page pageNumber={1} width={containerWidth} renderTextLayer renderAnnotationLayer />
+				</Document>
+			</TransformComponent>
+		</div>
 	);
 };
 
@@ -44,13 +51,13 @@ const TitleBar = () => {
 	const isMobile = window?.width <= 768;
 
 	return (
-		<div className="title-bar-container flex-col border-b">
-			<div className="resume title-bar flex items-start justify-between text-muted-foreground">
+		<div className="title-bar-container flex-col border-b relative">
+			<div className="resume title-bar">
 				<div className="resume-group">
-				<WindowControls title={WINDOW_IDS.RESUME} />
+					<WindowControls title={WINDOW_IDS.RESUME} />
 					<button className="ml-3 mr-1">
-						<PanelLeftIcon className="py-0 px-1 w-7 h-7"/>
-						<ChevronDown className="py-0 px-1 w-5 h-5"/>
+						<PanelLeftIcon className="py-0 px-1 w-7 h-7" />
+						<ChevronDown className="py-0 px-1 w-5 h-5" />
 					</button>
 					<div className="flex flex-col items-start justify-center">
 						<h3 className="text-sm font-medium text-accent-foreground">resume.pdf</h3>
@@ -58,23 +65,23 @@ const TitleBar = () => {
 					</div>
 				</div>
 				<div className="resume-group">
-					{/* <Controls /> */}
 					<button>
 						<Info />
 					</button>
-					<button>
-						<ZoomIn />
-					</button>
-					<button>
-						<ZoomOut />
-					</button>
+					<Controls />
 					<button>
 						<Share />
 					</button>
 					<a href="files/Pin_Horputra_Resume.pdf" download>
 						<CircleArrowDown />
 					</a>
-					{isMobile ? <button><Search /></button> : <input type="text" className="border border-muted-foreground/20" placeholder="Search" />}
+					{isMobile ? (
+						<button>
+							<Search />
+						</button>
+					) : (
+						<input type="text" className="border border-muted-foreground/20" placeholder="Search" />
+					)}
 				</div>
 			</div>
 			<div className="resume-group text-center text-xs">resume.pdf</div>
@@ -84,16 +91,14 @@ const TitleBar = () => {
 
 const ResumeWindow = () => {
 	return (
-		// <TransformWrapper initialScale={1} initialPositionX={200} initialPositionY={100}>
-		// 	{({ zoomIn, zoomOut, ...rest }) => {
-		Window({
-			id: WINDOW_IDS.RESUME,
-			title: apps[WINDOW_IDS.RESUME].label,
-			children: <Resume />,
-			customizeTitleBar: <TitleBar />,
-		})
-		// 	}}
-		// </TransformWrapper>
+		<TransformWrapper minScale={0.5} maxScale={4} initialScale={1} smooth={true} wheel={{ wheelDisabled: false, step: 1, smoothStep: 0.01 }} panning={{ velocityDisabled: true, wheelPanning: true }}>
+			{Window({
+				id: WINDOW_IDS.RESUME,
+				title: apps[WINDOW_IDS.RESUME].label,
+				children: <Resume />,
+				customizeTitleBar: <TitleBar />,
+			})}
+		</TransformWrapper>
 	);
 };
 
